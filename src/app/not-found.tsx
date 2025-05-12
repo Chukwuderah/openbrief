9bf3e7b6ac7642d607f208f3c3faa07b965f962b
Chukwuderah <pleasantchukwu@gmail.com>
2025-05-12 16:01:26 +0100

    fix: replace anchor tage with nextjs built in Link

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,6 +2,9 @@
 
 import { useEffect } from "react";
 import { usePathname } from "next/navigation";
+import { Button } from "@/components/ui/button";
+import { ArrowRight } from "lucide-react";
+import Link from "next/link";
 
 const NotFound = () => {
   const pathname = usePathname();
@@ -18,9 +21,15 @@ const NotFound = () => {
       <div className="text-center">
         <h1 className="text-4xl font-bold mb-4">404</h1>
         <p className="text-xl mb-4">Oops! Page not found</p>
-        <a href="/" className="text-blue-500 hover:text-blue-700 underline">
-          Return to Home
-        </a>
+        <Link href="/" prefetch>
+          <Button
+            size="lg"
+            className="group bg-indigo-600 hover:bg-indigo-700 text-white dark:bg-indigo-500 dark:hover:bg-indigo-600 cursor-pointer"
+          >
+            Return to Home
+            <ArrowRight className="ml-2 h-4 w-4 transition-transform group-hover:translate-x-1" />
+          </Button>
+        </Link>
       </div>
     </div>
   );
